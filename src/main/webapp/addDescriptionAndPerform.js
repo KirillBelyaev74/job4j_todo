ce8be8012f32849d7e125a1a73c1f5929b74978a
kirillbelyaev74 <kirbel74@gmail.com>
2021-04-12 00:24:03 +0500

    2. Создать TODO list[3786#449928]

--- src/main/webapp/addDescriptionAndPerform.js
+++ src/main/webapp/addDescriptionAndPerform.js
@@ -21,3 +21,29 @@ $(document).ready(function () {
         }
     })
 });
+
+$(document).ready(function () {
+    $("#perform").on("click", function () {
+        let values = [];
+        $('[name="item"]:checked').each(function () {
+            values.push($(this).val());
+        });
+        if (values.length !== 0) {
+            let itemsId = values.join(",");
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8080/job4j_todo/completed",
+                dataType: "json",
+                data: {"itemsId": itemsId}
+            }).done(function (data) {
+                let stringJson = JSON.stringify(data);
+                let answer = JSON.parse(stringJson);
+                let message = answer.text;
+                alert(message);
+                window.location.href = "http://localhost:8080/job4j_todo/";
+            }).fail(function (error) {
+                alert("Что то пошло не так " + error.val())
+            });
+        }
+    })
+});
